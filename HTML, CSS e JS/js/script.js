6e1c3c6823eeaadbc91124e21f24cafadcd70690
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-01-31 10:24:24 -0200

    reorganização das pastas e início do projeto de calendário

---
+++ HTML, CSS e JS/js/script.js
@@ -0,0 +1,6 @@
+var hoje = new Date();
+var ano = hoje.getFullYear();
+var mes = hoje.getMonth();
+var dia = hoje.getDate();
+var diaDaSemana = hoje.getDay();
+var primeiroDiaDoMes = new Date(ano, mes, 1);
